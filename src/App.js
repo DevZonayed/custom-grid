435ec90fc55e3cacd3fc5aee2691d14e7ceedc5e
DevZonayed <zonayed320@gmail.com>
2022-12-02 18:29:53 +0600

    initial commeit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import DataGrid from "./components/DataGrid/DataGrid";
+const App = () => {
+  return (
+    <>
+      <DataGrid />
+    </>
+  );
+};
+
+export default App;
